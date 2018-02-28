60a6d0dc09978d330e39d30325db17f7e429aead
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2018-02-28 11:51:12 -0600

    update to django 1.11

--- enterpriseback/enterpriseback/settings.py
+++ enterpriseback/enterpriseback/settings.py
@@ -1,23 +1,7 @@
 #!/usr/bin/env python
-"""
-Django settings for enterpriseback project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from enterpriseback.local_set import *
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
 
 # Application definition
 
--- enterpriseback/enterpriseback/urls.py
+++ enterpriseback/enterpriseback/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^djangoadmin/', include(admin.site.urls)),
     url(r'^', include("home.urls"))
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
